16f367ecc60c07b05a91a89d1065db476cb669e3
ging-dev <gingdz11012001@gmail.com>
2021-10-03 18:17:41 +0700

    Add box.json

--- Tests/GrammarTest.php
+++ Tests/GrammarTest.php
@@ -1,14 +1,17 @@
 <?php
 
-
-$grammar = new GingTeam\Grammar();
-
-test('regex test 1', function () use ($grammar) {
-    $data = file_get_contents(__DIR__.'/Fixtures/test.png');
-    $this->assertCount(1, $grammar->findAll($data));
+beforeEach(function() {
+    $this->grammar = new GingTeam\Grammar();
 });
 
-test('regex test 2', function () use ($grammar) {
-    $data = file_get_contents(__DIR__.'/Fixtures/HA3.bin');
-    $this->assertCount(447, $grammar->findAll($data));
+test('regex test 1', function () {
+    $this->assertCount(1, $this->grammar->findAll(getFixture('test.png')));
 });
+
+test('regex test 2', function () {
+    $this->assertCount(447, $this->grammar->findAll(getFixture('HA3.bin')));
+});
+
+function getFixture(string $filename): string {
+    return file_get_contents(__DIR__.'/Fixtures/'.$filename) ?? '';
+}
